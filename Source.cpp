1dcb4359591c867f60de3ac9115bb9c7ef39d7a5
Lidija Sokolova <Lidija.Sokolova@edu.rtu.lv>
2023-10-17 22:57:22 +0300

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,73 @@
+﻿#undef UNICODE   // Pārslēgt projektu ASCII režīmā 
+#include <windows.h> 
+#include <stdio.h> 
+#include "stdio.h"//stdio
+#include "resource.h"  
+#pragma warning(disable : 4996)  // Ziņojumu apstrādātāja prototips 
+BOOL CALLBACK MainWndProc(HWND, UINT, WPARAM, LPARAM);  
+
+// Galvenā funkcija  
+// // ja galvenā loga šablonam ir piešķirts resursa identifikators IDD_MAINDIALOG 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {  
+	DialogBox(   
+		hInstance,    
+		MAKEINTRESOURCE(IDD_MAINDIALOG),    
+		NULL,    
+		DLGPROC(MainWndProc)  );  
+	return 0; 
+}  // Ziņojumu apstrādātājs 
+
+void ShowInfo(HWND wnd) {
+	// Computer Name
+	char buf[100];
+	DWORD size = 100;
+	if (!GetComputerName(buf, &size)) strcpy_s(buf, "error");
+	SetDlgItemText(wnd, IDC_COMPNAME, buf);
+	// User Name
+	if (!GetUserName(buf, &size)) strcpy_s(buf, "error");
+	SetDlgItemText(wnd, IDC_USERNAME, buf);
+	// OS version
+	OSVERSIONINFO ver;
+	ver.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+	GetVersionEx(&ver);
+	sprintf_s(buf, "%d.%d (build %d) %s", ver.dwMajorVersion, ver.dwMinorVersion, ver.dwBuildNumber, ver.szCSDVersion);
+	SetDlgItemText(wnd, IDC_VERSION, buf);
+	// Physical memory
+	MEMORYSTATUSEX statex;
+	statex.dwLength = sizeof(statex);
+	GlobalMemoryStatusEx(&statex);
+	sprintf_s(buf, "%I64d MB", statex.ullTotalPhys / 1024/1024);
+	SetDlgItemText(wnd, IDC_TOTALRAM, buf);
+	// Free memory
+	sprintf_s(buf, "%I64d MB", statex.ullAvailPhys / 1024 / 1024);
+	SetDlgItemText(wnd, IDC_FREERAM, buf);
+	// Percent of memory in use. GlobalMemoryStatusEx
+	sprintf_s(buf, "%I64d MB", statex.dwMemoryLoad / 1024 / 1024);
+	SetDlgItemText(wnd, IDC_PERCENTRAM, buf);
+	// Current Date and Time. GetLocalTime
+	SYSTEMTIME sm;
+	GetLocalTime(&sm);
+	sprintf_s
+	SetDlgItemText(wnd, IDC_DATETIME, buf);
+	// Time since power on. GetTickCount
+}
+
+BOOL CALLBACK MainWndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {  
+	switch (Msg) { // pārbaudīt ziņojuma identifikatoru  
+	case WM_INITDIALOG: // ja tas ir inicializācijas paziņojums     
+		SetTimer(hWnd, NULL, 200, NULL);
+		ShowInfo(hWnd);				// izpildīt inicializācijas darbības   
+		return TRUE;  
+	case WM_COMMAND: // ja tas ir paziņojums no vadības elementa   
+		switch (LOWORD(wParam)) { // pārbaudīt vadības elementa ID   
+		case IDOK: // ja bija uzspiesta Close poga    
+			DestroyWindow(hWnd); // aizvērt logu    
+			return TRUE;  
+		}   
+		return FALSE;   
+	case WM_DESTROY: // ja tas ir pabeigšanas pieprasījums   
+		PostQuitMessage(0); // pabeigt programmu   
+		return TRUE;  
+	}  
+	return FALSE; 
+} 
---
+++ resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by CompInfo.rc
+//
+#define IDD_MAINDIALOG                  5
+#define IDC_COMPNAME                    1001
+#define IDC_USERNAME                    1002
+#define IDC_VERSION                     1003
+#define IDC_TOTALRAM                    1004
+#define IDC_FREERAM                     1005
+#define IDC_PERCENTRAM                  1006
+#define IDC_DATETIME                    1007
+#define IDC_TIMEON                      1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
